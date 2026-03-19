8438ec164c1fc3d4fc6d2bca57e0be4fa4c99213
Oceane01 <ocea.ducl@gmail.com>
2026-03-19 10:44:08 +0100

    exercice 5

--- exo1/script.py
+++ exo1/script.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+
 
 
 
@@ -37,24 +39,38 @@ tab = np.column_stack((tab, tdelta))
 # print(tab)
 
 # Exercice 4
-
-for i in range(6):
-    val = 2017 +i
-    tmin = np.genfromtxt('data/'+str(val)+'/tmin.csv', delimiter=',').tolist()
-    tmax = np.genfromtxt('data/'+str(val)+'/tmax.csv', delimiter=',').tolist()
-    tmoy = np.genfromtxt('data/'+str(val)+'/tmoy.csv', delimiter=',').tolist()
-    # print("Tmin :", tmin)
-    # print("Tmax :", tmax)
-    # print("Tmoy :", tmoy)
+tableaux ={}
+tableaux[2016] = tab
+for annee in range(2017, 2023):
+    tmin = np.genfromtxt(f'data/{annee}/tmin.csv', delimiter=',').tolist()
+    tmax = np.genfromtxt(f'data/{annee}/tmax.csv', delimiter=',').tolist()
+    tmoy = np.genfromtxt(f'data/{annee}/tmoy.csv', delimiter=',').tolist()
 
     tmin_array = np.array(tmin)
     tmax_array = np.array(tmax)
     tmoy_array = np.array(tmoy)
 
-    tab = np.column_stack((tmin_array, tmax_array, tmoy_array))
-    tdelta = tab[:, 1] - tab[:, 0]
+    tab_annee = np.column_stack((tmin_array, tmax_array, tmoy_array))
+    tdelta = tab_annee[:, 1] - tab_annee[:, 0]
+    tab_annee = np.column_stack((tab_annee, tdelta))
 
-    tab = np.column_stack((tab, tdelta))
-    print(tab)
+    tableaux[annee] = tab_annee  
 
-# Exercice 5
+# Exercice 5 - Statistiques par année
+
+for annee, tab_annee in tableaux.items():
+    tmax_annee        = np.max(tab_annee[:, 1])
+    jour_plus_chaud   = np.argmax(tab_annee[:, 1])
+    tmin_jour_chaud   = tab_annee[jour_plus_chaud, 0]
+    tmoy_annee        = np.mean(tab_annee[:, 2])
+    tmoy_delta        = np.mean(tab_annee[:, 3])
+
+
+print(tmax_annee,tmin_jour_chaud, tmoy_annee, tmoy_delta)
+
+
+# Exercice 6
+
+
+# Exercice 7
+
