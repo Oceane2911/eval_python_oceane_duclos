2e264643395b948afde78c78f3695f669e60c0af
Oceane01 <ocea.ducl@gmail.com>
2026-03-19 10:19:59 +0100

    exercice 4

--- exo1/script.py
+++ exo1/script.py
@@ -8,9 +8,9 @@ os.chdir(r"C:\Ecole\Python\eval\exo1")
 
 
 # Exercice 1
-tmin = np.genfromtxt('tmin.csv', delimiter=',').tolist()
-tmax = np.genfromtxt('tmax.csv', delimiter=',').tolist()
-tmoy = np.genfromtxt('tmoy.csv', delimiter=',').tolist()
+tmin = np.genfromtxt('data/2016/tmin.csv', delimiter=',').tolist()
+tmax = np.genfromtxt('data/2016/tmax.csv', delimiter=',').tolist()
+tmoy = np.genfromtxt('data/2016/tmoy.csv', delimiter=',').tolist()
 
 # print("Tmin :", tmin)
 # print("Tmax :", tmax)
@@ -29,4 +29,32 @@ tab = np.column_stack((tmin_array, tmax_array, tmoy_array))
 
 # Execerci 3
 
-tdelta 
+
+tdelta = tab[:, 1] - tab[:, 0]
+
+tab = np.column_stack((tab, tdelta))
+
+# print(tab)
+
+# Exercice 4
+
+for i in range(6):
+    val = 2017 +i
+    tmin = np.genfromtxt('data/'+str(val)+'/tmin.csv', delimiter=',').tolist()
+    tmax = np.genfromtxt('data/'+str(val)+'/tmax.csv', delimiter=',').tolist()
+    tmoy = np.genfromtxt('data/'+str(val)+'/tmoy.csv', delimiter=',').tolist()
+    # print("Tmin :", tmin)
+    # print("Tmax :", tmax)
+    # print("Tmoy :", tmoy)
+
+    tmin_array = np.array(tmin)
+    tmax_array = np.array(tmax)
+    tmoy_array = np.array(tmoy)
+
+    tab = np.column_stack((tmin_array, tmax_array, tmoy_array))
+    tdelta = tab[:, 1] - tab[:, 0]
+
+    tab = np.column_stack((tab, tdelta))
+    print(tab)
+
+# Exercice 5
